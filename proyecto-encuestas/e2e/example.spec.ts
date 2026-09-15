326e62c7c1dd520953ab52a624fc620070b313eb
Jcv47 <juancamilovictoriacobo@MacBook-de-Juan-Camilo-Victoria.local>
2026-09-15 03:12:03 -0500

    fix: cambiar click por toBeVisible en el texto principal

--- proyecto-encuestas/e2e/example.spec.ts
+++ proyecto-encuestas/e2e/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:8000/');
-  await page.getByText('Sondar: Innovación a tu Alcance Transformamos tus ideas en realidad digital con').click();
+  await expect(page.getByText('Sondar: Innovación a tu Alcance Transformamos tus ideas en realidad digital con')).toBeVisible();
   await page.getByRole('link', { name: 'Iniciar Sesión' }).click();
   await page.getByRole('textbox', { name: 'Correo:' }).click();
   await page.getByRole('textbox', { name: 'Correo:' }).fill('admin');
