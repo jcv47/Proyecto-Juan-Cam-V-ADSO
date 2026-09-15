b754e92fa3c9878bec0ea0698a7bce4a13fd11d7
Jcv47 <juancamilovictoriacobo@MacBook-de-Juan-Camilo-Victoria.local>
2026-09-15 02:38:25 -0500

    Fix: configurado

--- proyecto-encuestas/playwright.config.ts
+++ proyecto-encuestas/playwright.config.ts
@@ -72,8 +72,9 @@ export default defineConfig({
 
  
   webServer: {
-  command: 'php artisan serve',
+  command: 'php artisan serve --port=8000',
   url: 'http://127.0.0.1:8000',
   reuseExistingServer: !process.env.CI,
+  timeout: 120 * 1000,
 },
 });
