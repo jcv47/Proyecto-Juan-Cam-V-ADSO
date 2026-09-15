51a22800e3511461309a32de463a6041096f586a
Jcv47 <juancamilovictoriacobo@MacBook-de-Juan-Camilo-Victoria.local>
2026-09-15 03:24:14 -0500

    fix: navegar directo a /login en test de playwright

--- proyecto-encuestas/e2e/example.spec.ts
+++ proyecto-encuestas/e2e/example.spec.ts
@@ -4,11 +4,8 @@ test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:8000/');
   await page.getByRole('link', { name: 'Iniciar Sesión' }).click();
   await page.getByRole('textbox', { name: 'Correo:' }).click();
-  await page.getByRole('textbox', { name: 'Correo:' }).fill('admin');
-  await page.getByRole('textbox', { name: 'Correo:' }).press('Alt+@');
   await page.getByRole('textbox', { name: 'Correo:' }).fill('admin@sondar.com');
   await page.getByRole('textbox', { name: 'Contraseña:' }).click();
-  await page.getByRole('textbox', { name: 'Contraseña:' }).press('CapsLock');
   await page.getByRole('textbox', { name: 'Contraseña:' }).fill('12345678');
   await page.getByRole('button', { name: 'Ingresar' }).click();
   await page.getByRole('link', { name: 'Respuestas' }).click();
