ab4d942082a2e2c8625531082673912680281ebc
Jcv47 <juancamilovictoriacobo@MacBook-de-Juan-Camilo-Victoria.local>
2026-09-15 02:54:32 -0500

    ci: iniciar laravel server manualmente en background

--- proyecto-encuestas/playwright.config.ts
+++ proyecto-encuestas/playwright.config.ts
@@ -71,10 +71,8 @@ export default defineConfig({
   ],
 
  
-  webServer: {
-  command: 'php artisan serve --host=127.0.0.1 --port=8000',
-  url: 'http://127.0.0.1:8000',
-  reuseExistingServer: !process.env.CI,
-  timeout: 120 * 1000,
-},
+ // webServer: {
+//   command: 'php artisan serve',
+//   url: 'http://127.0.0.1:8000',
+// },
 });
